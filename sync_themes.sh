670e4254a4c6cfef5469b8c99fd9c7c4e179aaa7
Gilman Callsen <gilman.callsen@gmail.com>
2019-02-04 16:25:45 -0500

    WIP sync themes no longer deals w/ zip files

--- sync_themes.sh
+++ sync_themes.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
+#crond restart
+
 # Download all themes in our S3 bucket
-echo "Sync all themese from S3 ..."
+echo "Sync all themes from S3 ..."
+# TODO make bucket set from environment
+# consider if the aws profile should be able to be overriden as well, is it
+# possible we'd want more than one profile?
 aws s3 sync s3://temp-rho-theme  /opt/jboss/keycloak/themes/
 
-# Go to our themes directory
-cd /opt/jboss/keycloak/themes
-DIRECTORY=$(pwd)
-echo "Now in $DIRECTORY"
-
-# Find all new `*.zip` files and unzip with `-o` flag to overrite existing.
-# delete zip after unzipped.
-for f in $DIRECTORY/*.zip; do
-  echo "Unzipping and overwriting $i"
-  unzip -o $f
-  echo "Removing $i"
-  rm $f
-done
+# # Go to our themes directory
+# cd /opt/jboss/keycloak/themes
+# DIRECTORY=$(pwd)
+# echo "Now in $DIRECTORY"
+#
+# # Find all new `*.zip` files and unzip with `-o` flag to overrite existing.
+# # delete zip after unzipped.
+# for f in $DIRECTORY/*.zip; do
+#   echo "Unzipping and overwriting $i"
+#   unzip -o $f
+#   echo "Removing $i"
+#   rm $f
+# done
